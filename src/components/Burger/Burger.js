8d7bc6d32ee2f1b556395ff18157452eae8d6582
Paul Merchant <letheon@gmail.com>
2020-07-27 11:05:47 +0800

    Lecture 117 -> 127

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Burger = (props) => {
+  return <div></div>;
+};
+
+Burger.propTypes = {};
+
+export default Burger;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const burgerBuilder = () => {
+  return (
+    <Fragment>
+      <div>Burger</div>
+      <div>Build Controls</div>
+    </Fragment>
+  );
+};
+
+export default burgerBuilder;
